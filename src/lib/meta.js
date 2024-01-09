7fa6e13c5cbb09bbcd865e09dc5f9a05a26c08d6
dangialiang <dangialiang@gmail.com>
2024-01-10 00:26:27 +0800

    meta基础操作

---
+++ src/lib/meta.js
@@ -0,0 +1,35 @@
+const path = require('path')
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
+
+const { version } = require(resolve('../../package.json'))
+
+function createDB(name) {
+    console.log(name)
+}
+
+function hasDB(name) {
+    console.log(name)
+}
+
+function updateDB(options = {}) {
+    console.log(options)
+}
+
+function removeDB(name) {
+    console.log()
+}
+
+function getDB(name) {
+    console.log(name)
+}
+
+module.exports = {
+    version,
+    createDB,
+    hasDB,
+    updateDB,
+    removeDB,
+    getDB,
+}
