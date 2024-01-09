42e74f2007ad628d0b541f358e6ca259a12e308c
dangialiang <dangialiang@gmail.com>
2024-01-10 00:28:45 +0800

    editor config

--- src/lib/meta.js
+++ src/lib/meta.js
@@ -1,35 +1,35 @@
-const path = require('path')
+const path = require("path");
 function resolve(dir) {
-    return path.join(__dirname, dir)
+  return path.join(__dirname, dir);
 }
 
-const { version } = require(resolve('../../package.json'))
+const { version } = require(resolve("../../package.json"));
 
 function createDB(name) {
-    console.log(name)
+  console.log(name);
 }
 
 function hasDB(name) {
-    console.log(name)
+  console.log(name);
 }
 
 function updateDB(options = {}) {
-    console.log(options)
+  console.log(options);
 }
 
 function removeDB(name) {
-    console.log()
+  console.log();
 }
 
 function getDB(name) {
-    console.log(name)
+  console.log(name);
 }
 
 module.exports = {
-    version,
-    createDB,
-    hasDB,
-    updateDB,
-    removeDB,
-    getDB,
-}
+  version,
+  createDB,
+  hasDB,
+  updateDB,
+  removeDB,
+  getDB,
+};
